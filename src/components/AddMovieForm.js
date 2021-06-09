0da046536f419357bbe87abff242528b0f5aebce
iwood55 <isaac.ewood@icloud.com>
2021-06-08 20:23:34 -0400

    mvp finished

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 
 import { Link, useHistory } from 'react-router-dom';
 
+const mapStateToProps = (state) => {
+    return({
+        movies: state.movies.movie
+    });
+}
+
 const AddMovieForm = (props) => {
     const { push } = useHistory();
 
@@ -23,7 +29,10 @@ const AddMovieForm = (props) => {
     }
 
     const handleSubmit = (e) => {
-    }
+        Object.assign(movie,{ id: props.movies });
+            props.dispatch(addMovie(movie));
+                push('/movies');
+        }
 
     const { title, director, genre, metascore, description } = movie;
     return(<div className="col">
@@ -67,4 +76,4 @@ const AddMovieForm = (props) => {
     </div>);
 }
 
-export default AddMovieForm;
+export default connect(mapStateToProps,{})(AddMovieForm)
